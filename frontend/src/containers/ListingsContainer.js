a07563ed46217def5bf1c45648f9d654750e6aeb
jchasse <jonathan.r.chasse@gmail.com>
2020-11-19 13:55:29 -0600

    Framing out ListingsContainer

---
+++ frontend/src/containers/ListingsContainer.js
@@ -0,0 +1,14 @@
+import React from 'react'
+
+
+class ListingsContainer extends React.Component {
+
+    render() {
+        return (
+            <div>
+                LisingsContainer
+            </div>
+        )
+    }
+}
+export default ListingsContainer
