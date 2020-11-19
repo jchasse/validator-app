95a19297c0ecda368ec6e1f7c7c8fe5ea852ce24
jchasse <jonathan.r.chasse@gmail.com>
2020-11-19 13:57:15 -0600

    Creating App class with ListingsContainer rendered

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,4 +1,22 @@
+import React from 'react'
 
+import ListingsContainer from './containers/ListingsContainer'
+
+
+
+class App extends React.Component {
+
+  render() {
+    return (
+      <div className="App">
+        <Navbar />
+        <Container class="col-md-6">
+          <ListingsContainer />
+        </Container>
+
+      </div>
+    )
+  }
 }
 
 export default App
