f1a381255a2f78908aaebe74103e0423be8eed96
jchasse <jonathan.r.chasse@gmail.com>
2020-11-19 13:51:48 -0600

    Add devTools extenstion

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -2,6 +2,8 @@ import React from 'react'
 import ReactDOM from 'react-dom'
 import {Provider} from 'react-redux'
 import {createStore} from 'redux'
+import { composeWithDevTools } from 'redux-devtools-extension'
+
 import App from './App'
 import rootReducer from './reducers/rootReducer'
 
