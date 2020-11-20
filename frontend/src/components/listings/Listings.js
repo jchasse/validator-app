8d7da2073e149195f88e195e508f90f986ad8ad1
jchasse <jonathan.r.chasse@gmail.com>
2020-11-20 12:09:27 -0600

    Set listings class component

---
+++ frontend/src/components/listings/Listings.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react'
+import Listing from './Listing'
+
+class Listings extends Component {
+    render() {
+        return (
+            <>
+            </>
+        )
+    }
+}
+
+export default Listings
