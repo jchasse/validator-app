72f85f8de69a86e5c0066e153a19411d6fe14c76
jchasse <jonathan.r.chasse@gmail.com>
2020-11-20 12:13:24 -0600

    Update Listing Container for showing listings and input form

--- frontend/src/containers/ListingsContainer.js
+++ frontend/src/containers/ListingsContainer.js
@@ -1,4 +1,8 @@
 import React from 'react'
+import {connect} from 'react-redux'
+import ListingInput from '../components/listings/ListingInput'
+import {addListing, deleteListing} from '../actions/listings'
+import Listings from '../components/listings/Listings'
 
 
 class ListingsContainer extends React.Component {
@@ -6,9 +10,20 @@ class ListingsContainer extends React.Component {
     render() {
         return (
             <div>
-                LisingsContainer
+                {/* LisingsContainer */}
+                <ListingInput addListing={this.props.addListing} />
+                <Listings listings={this.props.listings} deleteListing={this.props.deleteListing}/>
             </div>
         )
     }
 }
-export default ListingsContainer
+
+const mapStateToProps = state => ({listings: state.listings})
+
+// const mapDispatchToProps = dispatch => {
+//     return {
+//         addListing: listing => dispatch({action: "ADD_LISTING", payload: listing})
+//     }
+// }
+
+export default connect(mapStateToProps, {addListing, deleteListing})(ListingsContainer)
