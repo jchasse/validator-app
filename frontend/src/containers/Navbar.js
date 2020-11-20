a66da4cd66e833df5709a6f68d8d3cee6fe8ee5e
jchasse <jonathan.r.chasse@gmail.com>
2020-11-20 16:11:09 -0600

    Created links on Navbar for listings and new

--- frontend/src/containers/Navbar.js
+++ frontend/src/containers/Navbar.js
@@ -1,17 +1,21 @@
 import React from 'react'
 import {Navbar, Nav, Form, FormControl, Button} from 'react-bootstrap'
+import { LinkContainer } from 'react-router-bootstrap'
 
  
-function NavBar(props){
+function NavBar(){
 
     return (
         <>
             <Navbar bg="dark" variant="dark">
                 <Navbar.Brand href="#home">REinder</Navbar.Brand>
-                <Nav className="mr-auto">
-                    <Nav.Link href="#home">Home</Nav.Link>
-                    <Nav.Link href="#features">Features</Nav.Link>
-                    <Nav.Link href="#pricing">Pricing</Nav.Link>
+                <Nav className="mr-auto" activeKey={window.location.pathname}>
+                    <LinkContainer to="/listings">
+                        <Nav.Link>Listings</Nav.Link>
+                    </LinkContainer>
+                    <LinkContainer to="/listings/new">
+                        <Nav.Link>New Listing</Nav.Link>
+                    </LinkContainer>
                 </Nav>
                 <Form inline>
                     <FormControl type="text" placeholder="Search" className="mr-sm-2" />
