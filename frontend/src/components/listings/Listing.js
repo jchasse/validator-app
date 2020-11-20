e9eb7286d616a96accc499bbbd46dc03e544d2c9
jchasse <jonathan.r.chasse@gmail.com>
2020-11-20 12:12:13 -0600

    Duplicated input card from Listing Form for showing

--- frontend/src/components/listings/Listing.js
+++ frontend/src/components/listings/Listing.js
@@ -5,6 +5,26 @@ import {Card, Button} from 'react-bootstrap'
 export default function Listing(props) {
     return (
         <>
+            <Card style={{ width: '20rem' }}>
+                <Card.Img variant="top" src="https://picsum.photos/800" />              
+                <Card.Body>
+                    <Card.Title>
+                        {props.listing.title}
+                    </Card.Title>
+                    <Card.Text>
+                        {props.listing.id} - {props.listing.content}
+                    </Card.Text>
+                    <Button 
+                        variant="secondary">
+                        Edit
+                    </Button>
+                    <Button 
+                        variant="danger"
+                        onClick={ () => props.deleteListing(props.listing.id)}>
+                        Delete
+                    </Button>
+                </Card.Body>
+            </Card>
         </>
     )
 }
