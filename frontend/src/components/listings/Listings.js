d63b0b2f925044d6ae71f3f2412792f6d19dad00
jchasse <jonathan.r.chasse@gmail.com>
2020-11-20 12:09:41 -0600

    Map function over listings

--- frontend/src/components/listings/Listings.js
+++ frontend/src/components/listings/Listings.js
@@ -3,8 +3,10 @@ import Listing from './Listing'
 
 class Listings extends Component {
     render() {
+        const listingsList = this.props.listings.map( listing => <Listing key={listing.id} listing={listing} deleteListing={this.props.deleteListing}/>)
         return (
             <>
+                {listingsList}
             </>
         )
     }
