d7d45d03a3644432e8970603d8b84ce9c6b253d4
jchasse <jonathan.r.chasse@gmail.com>
2020-11-23 15:03:18 -0600

    Refactoring listing card and adding in delete and edit button functionality

--- frontend/src/components/listings/Listing.js
+++ frontend/src/components/listings/Listing.js
@@ -1,18 +1,47 @@
 import React from 'react'
 import {Card, Button} from 'react-bootstrap'
+import {withRouter} from 'react-router-dom'
 
 
-export default function Listing(props) {
+function Listing(props) {
+
+    const handleOnDelete = () => {
+        props.deleteListing(props.listing.id)
+        props.history.push('/listings')
+      }
+
     return (
         <>
-            <Card style={{ width: '20rem' }}>
-                <Card.Img variant="top" src="https://picsum.photos/800" />              
+            <Card>
+                <Card.Img variant="top" src="https://picsum.photos/800/200" />              
                 <Card.Body>
                     <Card.Title>
                         {props.listing.title}
                     </Card.Title>
                     <Card.Text>
                         {props.listing.id} - {props.listing.content}
+                    </Card.Text>
+                    <Button 
+                        variant="secondary"
+                        onClick={ () => props.history.push(`/listings/${props.listing.id}/edit`)} >
+                        Edit
+                    </Button>
+                    <Button 
+                        variant="danger"
+                        onClick={ () => handleOnDelete()}>
+                        Delete
+                    </Button>
+                </Card.Body>
+            </Card>
+
+
+            {/* <Card>
+                <Card.Img variant="top" src="https://picsum.photos/800/200" />
+                <Card.Header as="h5">{props.listing.title}</Card.Header>
+                <Card.Body>
+                    <Card.Title>Special title treatment</Card.Title>
+                    <Card.Text>
+                    {props.listing.content}
                     </Card.Text>
                     <Button 
                         variant="secondary">
@@ -24,7 +53,11 @@ export default function Listing(props) {
                         Delete
                     </Button>
                 </Card.Body>
-            </Card>
+            </Card> */}
+
         </>
     )
 }
+
+
+export default withRouter(Listing)
