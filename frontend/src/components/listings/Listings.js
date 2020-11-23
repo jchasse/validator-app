f4bd6eea549769e2e65548a4fede8aaa17fec77d
jchasse <jonathan.r.chasse@gmail.com>
2020-11-23 14:59:36 -0600

    Styling Listings container and removing unused bootstrap

--- frontend/src/components/listings/Listings.js
+++ frontend/src/components/listings/Listings.js
@@ -1,12 +1,14 @@
 import React, { Component } from 'react'
 import Listing from './Listing'
+// import {CardDeck, CardColumns} from 'react-bootstrap'
+
 
 class Listings extends Component {
     render() {
         const listingsList = this.props.listings.map( listing => <Listing key={listing.id} listing={listing} deleteListing={this.props.deleteListing}/>)
         return (
             <>
-                {listingsList}
+            {listingsList}
             </>
         )
     }
