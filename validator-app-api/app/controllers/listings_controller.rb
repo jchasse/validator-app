9d98bb7c50446b9355846e6576f78455cd566fa0
jchasse <jonathan.r.chasse@gmail.com>
2020-11-24 11:22:18 -0600

    Added in create route in listings controller on backend

--- validator-app-api/app/controllers/listings_controller.rb
+++ validator-app-api/app/controllers/listings_controller.rb
@@ -4,5 +4,17 @@ class ListingsController < ApplicationController
         listings = Listing.all
         render json: ListingSerializer.new(listings)
     end
-    
+
+    def create
+        listing = Listing.create(listing_params)
+        render json: ListingSerializer.new(listing)
+    end
+
+
+    private
+
+    def listing_params
+        params.require(:listing).permit(:title, :city, :state, :content, :price, :cashflow, :link)
+    end
+
 end
