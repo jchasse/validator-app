1301a3b1a3f1e9c261a78bfc7fa3a18aa69262b7
jchasse <jonathan.r.chasse@gmail.com>
2020-11-24 10:40:55 -0600

    Added in thunk middleware

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -3,13 +3,14 @@ import ReactDOM from 'react-dom'
 import {BrowserRouter as Router} from 'react-router-dom'
 
 import {Provider} from 'react-redux'
-import {createStore} from 'redux'
+import {createStore, compose, applyMiddleware} from 'redux'
+import thunk from 'redux-thunk'
 import {composeWithDevTools} from 'redux-devtools-extension'
 
 import App from './App'
 import rootReducer from './reducers/rootReducer'
 
-const store = createStore( rootReducer, composeWithDevTools() )
+const store = createStore( rootReducer, compose( applyMiddleware(thunk), composeWithDevTools() ) )
 
 ReactDOM.render(
   <Router>
