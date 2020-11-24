f70d44c5a2ba44cd674bc5e58fc813e31d80cb0d
jchasse <jonathan.r.chasse@gmail.com>
2020-11-24 09:42:21 -0600

    Adding seed data for DB

--- validator-app-api/db/seeds.rb
+++ validator-app-api/db/seeds.rb
@@ -5,3 +5,17 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+listing = Listing.create({
+    title: "Great San Antonio pool route",
+    city: "San Antonio",
+    state: "TX",
+    content: "The perfect route for a buyer that has little or no pool experience. It consists of 35 accounts generating a guaranteed $6,500 per month plus income from repairs. During the current pandemic, pool companies have been classified as essential because of the requirement to treat the water to prevent mosquitoes.",
+    price: "110,000",
+    cashflow: "100,000",
+    link: "https://www.bizbuysell.com/Business-Opportunity/great-san-antonio-pool-route/1769169?d=L3RleGFzLWJ1c2luZXNzZXMtZm9yLXNhbGU=",
+    votes: 22
+})
+
+
+
