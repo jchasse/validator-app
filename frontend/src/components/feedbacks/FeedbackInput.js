dac65dae16c72f1c9d965d6b7494b2df435e92ef
jchasse <jonathan.r.chasse@gmail.com>
2020-12-01 19:39:21 -0600

    Connected to the store functions of add and edit feedback

--- frontend/src/components/feedbacks/FeedbackInput.js
+++ frontend/src/components/feedbacks/FeedbackInput.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react'
 import {connect} from 'react-redux'
+import { addFeedback, editFeedback} from '../../actions/feedbacks'
 
 
 class FeedbackInput extends Component {
@@ -7,11 +8,53 @@ class FeedbackInput extends Component {
     constructor({id, comment, listingId}) {
         super()
         this.state = {
+            id: (id ? id : ''),
+            comment: (comment ? comment : ''),
+            listingId: (listingId ? listingId : '')
         }
     }
 
+    handleOnChange(event) {
+        this.setState({[event.target.name]: event.target.value})
+    }
+        
+    handleOnSubmit(event) {
+        event.preventDefault()
+        if (!this.props.id) {
+            this.props.addFeedback(this.state)
+        } else {
+            this.props.editFeedback(this.state)
+            this.props.editOrSubmit()
+        }
+        this.setState({
+            id: '',
+            comment: '',
+            listing_id: ''
+        })
+    }
 
     render() {
+        const editOrSubmit = () => !this.props.id ? 'Submit' : 'Update'
+        return (
+            <ListGroupItem>
+                <Form onSubmit={(event) => this.handleOnSubmit(event)}> 
+                        <Row>
+                            <Col>
+                                <Form.Control
+                                className="mb-2 mr-sm-4" 
+                                name="comment" 
+                                value={this.state.comment}
+                                placeholder="Comment"
+                                onChange={(event) => this.handleOnChange(event)} />
+                            </Col>
+                            <Col xs={2}>
+                                <Button className="float-right" variant="outline-secondary" size="sm" type="submit" > {editOrSubmit()} </Button>
+                            </Col>
+                        </Row>
+                </Form>
+            </ListGroupItem>
+        )
+    }
 }
 
 export default connect(null, {addFeedback, editFeedback})(FeedbackInput)
