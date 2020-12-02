944baa5d1cddb43b66ebc2a302ee484cfe6d6e08
jchasse <jonathan.r.chasse@gmail.com>
2020-12-01 19:50:59 -0600

    Removed and refactored card presentation for listing

--- frontend/src/components/listings/Listing.js
+++ frontend/src/components/listings/Listing.js
@@ -14,75 +14,8 @@ function Listing(props) {
     return (
         <>
             <Card>
-                <Card.Body>
-                    <Container> 
-                        <Row>
-                            <Image src="https://mdbootstrap.com/img/Photos/Avatars/avatar-8.jpg" alt="avatar" roundedCircle/>  
-                            <div>
-                                <Card.Title>
-                                    {props.listing && props.listing.title}
-                                </Card.Title>  
-                                <Card.Text>
-                                    <p className="card-text"><i class="far fa-clock pr-2"></i>{props.listing && props.listing.created_at}</p>
-                                </Card.Text>
-                            </div>
-                        </Row>
-                    </Container>
-                    <Container>
-                        <Card.Img variant="top" src="https://picsum.photos/800/200" />   
-                        <Card.Text>
-                            {props.listing && props.listing.content}
-                        </Card.Text>
-                    </Container>
-                    <Button 
-                        variant="secondary"
-                        onClick={ () => props.history.push(`/listings/${props.listing.id}/edit`)} >
-                        Edit
-                    </Button>
-                    <Button 
-                        variant="danger"
-                        onClick={ () => handleOnDelete()}>
-                        Delete
-                    </Button>
                 </Card.Body>
-                    <Card.Footer className="text-muted">
-                        <Button
-                            className="btn btn-secondary"
-                            onClick={ () => props.upvoteListing(props.listing)}>
-                            +
-                        </Button>
-                        <span> Votes: {props.listing && props.listing.votes} </span>
-                        <Button
-                            className="btn btn-secondary"
-                            onClick={ () => props.downvoteListing(props.listing)}>
-                            -
-                        </Button>                        
-                    </Card.Footer>
-
-
             </Card>
-
-
-            {/* <Card>
-                <Card.Img variant="top" src="https://picsum.photos/800/200" />
-                <Card.Header as="h5">{props.listing.title}</Card.Header>
-                <Card.Body>
-                    <Card.Title>Special title treatment</Card.Title>
-                    <Card.Text>
-                    {props.listing.content}
-                    </Card.Text>
-                    <Button 
-                        variant="secondary">
-                        Edit
-                    </Button>
-                    <Button 
-                        variant="danger"
-                        onClick={ () => props.deleteListing(props.listing.id)}>
-                        Delete
-                    </Button>
-                </Card.Body>
-            </Card> */}
-
         </>
     )
 }
