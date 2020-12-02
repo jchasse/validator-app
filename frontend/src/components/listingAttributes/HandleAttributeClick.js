65d1ce1751de7d82f971514efdbee6a20519d498
jchasse <jonathan.r.chasse@gmail.com>
2020-12-01 19:30:26 -0600

    Added handle attribute click function to support buttons for user feedback

---
+++ frontend/src/components/listingAttributes/HandleAttributeClick.js
@@ -0,0 +1,13 @@
+// import React, {Component} from 'react'
+// import {connect} from 'react-redux'
+// import {upvoteListing} from '../actions/listings'
+
+// function handleAttributeClick(listing) {
+    
+//     props.upvoteListing(listing)
+
+// }
+
+// export default connect(null, {upvoteListing})(handleAttributeClick)
+
+// export default connect(null, {deleteList})(List)
