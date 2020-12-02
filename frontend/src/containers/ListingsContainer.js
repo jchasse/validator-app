37f8704e728d89acac3b208e071e5bce69cbaf64
jchasse <jonathan.r.chasse@gmail.com>
2020-12-01 19:46:20 -0600

    Refactored to remove un-necessary prop drilling, specifically action functions

--- frontend/src/containers/ListingsContainer.js
+++ frontend/src/containers/ListingsContainer.js
@@ -5,7 +5,7 @@ import {Route, Switch} from 'react-router-dom'
 import ListingInput from '../components/listings/ListingInput'
 import Listings from '../components/listings/Listings'
 import Listing from '../components/listings/Listing'
-import {fetchListings, addListing, deleteListing, editListing, upvoteListing, downvoteListing} from '../actions/listings'
+import {fetchListings, addListing, deleteListing, editListing} from '../actions/listings'
 
 class ListingsContainer extends React.Component {
 
@@ -18,24 +18,21 @@ class ListingsContainer extends React.Component {
             <>
                 <Switch>
                     <Route exact path='/listings/new'>
-                        <ListingInput addListing={this.props.addListing} />    
+                        <ListingInput />    
                     </Route>
                     <Route exact path='/listings/:id' render={ routerProps => <Listing 
                         {...routerProps} 
                         listing={this.props.listings.find( listing => listing.id === routerProps.match.params.id)} 
-                        deleteListing={this.props.deleteListing}
                         />} 
                     />
                     <Route exact path='/listings/:id/edit' render={ routerProps => <ListingInput 
                         {...routerProps} 
                         listing={this.props.listings.find( listing => listing.id === routerProps.match.params.id)}
-                        editListing={this.props.editListing}  
-                        deleteListing={this.props.deleteListing}
                         />} 
                     />
                     <Route exact path='/listings'>
-                        <ListingInput addListing={this.props.addListing} />    
-                        <Listings listings={this.props.listings} deleteListing={this.props.deleteListing} upvoteListing={this.props.upvoteListing} downvoteListing={this.props.downvoteListing} />   
+                        {/* <Listings /> */}
+                        <Listings listings={this.props.listings} />   
                     </Route>
                 </Switch>
             </>
@@ -45,4 +42,4 @@ class ListingsContainer extends React.Component {
 
 const mapStateToProps = state => ({listings: state.listings})
 
-export default connect(mapStateToProps, {fetchListings, addListing, deleteListing, editListing, upvoteListing, downvoteListing})(ListingsContainer)
+export default connect(mapStateToProps, {fetchListings, addListing, deleteListing, editListing})(ListingsContainer)
