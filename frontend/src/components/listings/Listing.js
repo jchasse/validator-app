74bda9efe195760f8375a03909d81d2f4b245019
jchasse <jonathan.r.chasse@gmail.com>
2020-12-01 19:53:02 -0600

    Card inputs from props, checking if props.listing exists

--- frontend/src/components/listings/Listing.js
+++ frontend/src/components/listings/Listing.js
@@ -19,6 +19,21 @@ function Listing(props) {
                 {/* <Card.Img variant="top" src="https://cdn.pixabay.com/photo/2016/06/24/10/47/architecture-1477041_1280.jpg" /> */}
                 <Card.Img variant="top" src={`${props.listing.link}`} />
 
+                <Card.Body> 
+                    <Card.Title> {props.listing && props.listing.title} </Card.Title>  
+                    <Row>
+                        <Col> <Card.Title> Asking Price: {props.listing && props.listing.price} </Card.Title> </Col>
+                        <Col> <Card.Title className="float-right"> Cashflow: {props.listing && props.listing.cashflow} </Card.Title> </Col>
+                    </Row>
+                    <Card.Text>{props.listing && props.listing.content}</Card.Text> 
+                    <Row>
+                        <Col> 
+                            <Card.Title>{props.listing && props.listing.city} - {props.listing && props.listing.state}</Card.Title> 
+                            </Col>
+                        <Col>
+                            <Card.Text> {/* {formattedDate} */} {/* {props.listing && props.listing.created_at} */} </Card.Text>                     
+                        </Col>
+                        <Col>
                 </Card.Body>
             </Card>
         </>
