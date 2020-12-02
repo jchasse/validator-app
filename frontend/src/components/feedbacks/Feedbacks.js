88e1a4cb1b14bb9ea5c62053f7d4270bf66f0578
jchasse <jonathan.r.chasse@gmail.com>
2020-12-01 19:36:20 -0600

    Mapping over store props to create a list of feedbacks under the listing

--- frontend/src/components/feedbacks/Feedbacks.js
+++ frontend/src/components/feedbacks/Feedbacks.js
@@ -3,6 +3,9 @@ import Feedback from './Feedback'
 
 
 function Feedbacks(props){ 
+    const feedbacksList = props.feedbacks.map( feedback => <Feedback key={feedback.id} feedback={feedback} />)
+
+    return <>{feedbacksList}</>
 }
 
 export default Feedbacks
