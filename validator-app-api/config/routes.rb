c72706f81b32fe5e3d52e44d153660c71c0fd738
jchasse <jonathan.r.chasse@gmail.com>
2020-12-01 19:19:00 -0600

    Created nested resources for feedback

--- validator-app-api/config/routes.rb
+++ validator-app-api/config/routes.rb
@@ -1,4 +1,11 @@
 Rails.application.routes.draw do
+    # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+
+  resources :feedbacks
   resources :listings
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+
+  resources :listings do
+    resources :feedbacks
+  end
+
 end
