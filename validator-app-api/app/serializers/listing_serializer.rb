ac93411d912cc960f755fefdc33d5e6b1381845f
jchasse <jonathan.r.chasse@gmail.com>
2020-12-01 19:19:28 -0600

    Created serializer for listing, Fast_JSON

--- validator-app-api/app/serializers/listing_serializer.rb
+++ validator-app-api/app/serializers/listing_serializer.rb
@@ -1,4 +1,6 @@
 class ListingSerializer
   include FastJsonapi::ObjectSerializer
-  attributes     :title, :city, :state, :content, :price, :cashflow, :link, :votes, :created_at
+  attributes     :title, :city, :state, :content, :price, :cashflow, :link, :votes, :created_at, :updated_at, :feedbacks
+  
+  # create a method to change the date/time format
 end
